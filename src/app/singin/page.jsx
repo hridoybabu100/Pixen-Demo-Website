d1eee32b8dc077de0788fb8564edf6410a114afb
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-04-28 20:21:38 +0600

    singup page updated

---
+++ src/app/singin/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div>
+            Sing In Page
+        </div>
+    );
+};
+
+export default SignIn;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -34,12 +34,12 @@ const Navbar = () => {
         </ul>
 
         <div className="flex gap-4">
-          <ul className="flex items-center  text-sm">
+          <ul className="flex items-center gap-2  text-sm">
             <li>
-              <Link href={"/signup"}>SignUp</Link>
+              <Link href={"/singup"}>SignUp</Link>
             </li>
             <li>
-              <Link href={"/signin"}>SignIn</Link>
+              <Link href={"/singin"}>SignIn</Link>
             </li>
           </ul>
         </div>
