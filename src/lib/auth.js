8b167b7920fc8d3f2ad6b71555c1b66ec920690c
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-04-28 19:33:01 +0600

    again updated

---
+++ src/lib/auth.js
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.PIXGEN_URL);
+const db = client.db('PixenDemo');
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+
+      emailAndPassword: { 
+    enabled: true, 
+  }, 
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+});
